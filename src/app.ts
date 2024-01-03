3cb1e9e32fae240cf3fa56c38a0a8148156334ca
aaronJau21 <aaronjau21@gmail.com>
2024-01-03 13:40:03 -0500

    created ENV

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import { Server } from "./presentation/server";
+// import { Server } from "./presentation/server";
+
+import { envs } from "./config/plugins/envs.plugins";
 
 (() => {
   main();
 })();
 
 function main() {
-  Server.start();
+  // Server.start();
+  console.log(envs);
 }
